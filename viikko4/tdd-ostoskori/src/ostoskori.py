56364d2908a6fcf61cca8b628845452e83213928
PurPul <axbergsamuel@gmail.com>
2022-11-28 19:00:56 +0200

    testi 3

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -16,7 +16,10 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
+        if self.__ostos is None:
+            return 0
+        else:
+            return self.__ostos.hinta()
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
