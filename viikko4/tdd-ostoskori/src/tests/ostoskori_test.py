8300acfa0f2696dae8b25f548e5dde904e3c5113
PurPul <axbergsamuel@gmail.com>
2022-11-28 19:13:43 +0200

    testi 6

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,31 +5,32 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito", 3)
+        self.juusto = Tuote("Juusto", 4)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
     
     def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteen_hinta(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.hinta(), 3)
     
     def test_kahden_eri_tuotteen_lisamisen_jalkeen_ostoskorissa_on_2_tavaraa(self):
-        maito = Tuote("Maito", 3)
-        juusto = Tuote("Juusto", 4)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(juusto)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.juusto)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
     
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteiden_hintojen_summa(self):
-        maito = Tuote("Maito", 3)
-        juusto = Tuote("Juusto", 4)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(juusto)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.juusto)
         self.assertEqual(self.kori.hinta(), 7)
+    
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_2_tavaraa(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
